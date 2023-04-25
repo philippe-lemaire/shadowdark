841008ff9ab2ca581ad20feee4690e7a0d0c48f3
Philippe Lemaire <philippe.lemaire@protonmail.com>
2023-04-25 12:35:06 +0200

    base routing ok

---
+++ generators/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+app_name = "generators"
+
+urlpatterns = [
+    # path("adventuring_party", views.adventuring_party_view, name="adventuring_party"),
+]
